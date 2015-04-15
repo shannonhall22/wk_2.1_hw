cee72e9c8d10c9d5165cba43ca4fd0e545a39886
shannonhall22 <shannonhall22@gmail.com>
2015-04-14 21:00:33 -0500

    first four hashes complete

--- app/controllers/hashes_controller.rb
+++ app/controllers/hashes_controller.rb
@@ -11,7 +11,7 @@ class HashesController < ApplicationController
     #===============================================
     book = { "binding" => "hardcover", "color" => "red", "num_pages" => 372}
 
-    @result = "Replace this string with your answer"
+    @result = book['num_pages']
     render 'hashes'
   end
 
@@ -24,7 +24,7 @@ class HashesController < ApplicationController
     #===============================================
     user = { "id" => 45, "email" => "name@example.com" }
 
-    @result = "Replace this string with your answer"
+    @result = user['id']
     render 'hashes'
   end
 
@@ -37,7 +37,7 @@ class HashesController < ApplicationController
     #===============================================
     item = { "name" => "bag", "brand" => "timbuk2", "price" => 79.00 }
 
-    @result = "Replace this string with your answer"
+    @result = item['price']
     render 'hashes'
   end
 
@@ -50,7 +50,7 @@ class HashesController < ApplicationController
     #===============================================
     fruit = { "name" => "durian", "smelly?" => true, "size" => "medium" }
 
-    @result = "Replace this string with your answer"
+    @result = fruit['size']
     render 'hashes'
   end
 
